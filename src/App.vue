b83485d5b2e4d082b5ea0697afdbbba2a85bf0cf
julianqrooz <julianqrooz@gmail.com>
2025-12-14 23:28:00 +0300

    Add closeMenu function to navbar for mobile

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@ import { useRouter } from "vue-router";
 import { useAuthStore } from "./stores/counter";
 const router = useRouter();
 const authuser = useAuthStore();
+const closeMenu = () => {
+  const navbar = document.getElementById("navbarSupportedContent");
+  if (navbar && navbar.classList.contains("show")) {
+    navbar.classList.remove("show");
+  }
+};
 </script>
 
 <template>
-  <!-- أهم حاجة: viewport في public/index.html، بس لو مش موجود أضفه هناك -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
     <div class="container px-4 px-lg-5">
-      <!-- العنوان -->
       <RouterLink class="navbar-brand fw-bold fs-4" to="/"
         >Julia Blog</RouterLink
       >
 
-      <!-- زر الهامبرجر للموبايل (ده اللي كان ناقص!) -->
       <button
         class="navbar-toggler"
         type="button"
@@ -27,20 +30,24 @@ const authuser = useAuthStore();
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <!-- المنيو -->
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+          <!-- Home -->
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
+            <router-link class="nav-link" to="/" @click="closeMenu"
+              >Home</router-link
+            >
           </li>
 
           <!-- لو مسجل دخول -->
           <template v-if="authuser.isLoggedIn">
             <li class="nav-item">
-              <router-link class="nav-link" to="/my-blog">My Blog</router-link>
+              <router-link class="nav-link" to="/my-blog" @click="closeMenu"
+                >My Blog</router-link
+              >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/create"
+              <router-link class="nav-link" to="/create" @click="closeMenu"
                 >Blog Writing</router-link
               >
             </li>
@@ -54,6 +61,7 @@ const authuser = useAuthStore();
                 class="btn btn-outline-light btn-sm"
                 @click="
                   authuser.logout();
+                  closeMenu();
                   router.push('/login');
                 "
               >
@@ -65,12 +73,15 @@ const authuser = useAuthStore();
           <!-- لو مش مسجل -->
           <template v-else>
             <li class="nav-item">
-              <router-link class="nav-link" to="/login">Log In</router-link>
+              <router-link class="nav-link" to="/login" @click="closeMenu"
+                >Log In</router-link
+              >
             </li>
             <li class="nav-item">
               <router-link
                 class="btn btn-outline-light btn-sm ms-2"
                 to="/register"
+                @click="closeMenu"
               >
                 Sign Up
               </router-link>
@@ -81,7 +92,6 @@ const authuser = useAuthStore();
     </div>
   </nav>
 
-  <!-- المحتوى الرئيسي -->
   <main class="flex-shrink-0 mt-5 pt-4">
     <router-view />
   </main>
