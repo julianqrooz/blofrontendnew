b2604b894381589854e6da0c00c09189f4876388
julianqrooz <julianqrooz@gmail.com>
2025-12-14 22:40:21 +0300

    Fix import path for useAuthStore in RegisterView

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { useAuthStore } from '../stores/auth'  // تأكد من المسار
+import { useAuthStore } from '../stores/counter'  // تأكد من المسار
 
 const router = useRouter()
 const authStore = useAuthStore()
